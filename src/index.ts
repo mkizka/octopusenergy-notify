145a29faa0f824906e4186b11fe75c8365f58eca
mkizka <g.mkizka@gmail.com>
2024-10-01 20:42:04 +0900

    月初にエラーになるのを修正

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 import { TZDate } from "@date-fns/tz";
-import { addDays, endOfMonth, format } from "date-fns";
+import { addDays, addSeconds, endOfMonth, format, startOfDay, startOfMonth } from "date-fns";
 
 import { postDiscordMessage } from "./discord.js";
 import { env } from "./env.js";
@@ -7,31 +7,40 @@ import { login } from "./login.js";
 import { fetchHalfHourlyReadings } from "./reading.js";
 import { _, sumByKey } from "./utils.js";
 
-const now = TZDate.tz("Asia/Tokyo");
-const yesterday = format(addDays(now, -1), "yyyy-MM-dd");
-const twoDaysAgo = format(addDays(now, -2), "yyyy-MM-dd");
-const firstDayOfThisMonth = new TZDate(now.getFullYear(), now.getMonth(), 1, "Asia/Tokyo");
-const remainingDaysOfThisMonth = endOfMonth(now).getDate() - now.getDate() + 1;
+const today = startOfDay(TZDate.tz("Asia/Tokyo"));
+const yesterday = addSeconds(today, -1);
+
+// eslint-disable-next-line no-console
+console.log({
+  today: today.toISOString(),
+  yesterday: yesterday.toISOString(),
+});
 
 const token = await login(env.OEJP_EMAIL, env.OEJP_PASSWORD);
 const result = await fetchHalfHourlyReadings({
   accountNumber: env.OEJP_ACCOUNT_NUMBER,
-  fromDatetime: firstDayOfThisMonth.toISOString(),
-  toDatetime: now.toISOString(),
+  fromDatetime: startOfMonth(yesterday).toISOString(),
+  toDatetime: yesterday.toISOString(),
   token,
 });
 
-const diffValue = result[yesterday].value - result[twoDaysAgo].value;
-const diffCostEstimate = result[yesterday].costEstimate - result[twoDaysAgo].costEstimate;
+// eslint-disable-next-line no-console
+console.log(result);
+
+const yesterdayKey = format(yesterday, "yyyy-MM-dd");
+const twoDaysAgoKey = format(addDays(yesterday, -1), "yyyy-MM-dd");
+const diffValue = result[yesterdayKey].value - result[twoDaysAgoKey].value;
+const diffCostEstimate = result[yesterdayKey].costEstimate - result[twoDaysAgoKey].costEstimate;
 const sumCostEstimate = sumByKey(Object.values(result), "costEstimate");
 const avgCostEstimate = sumCostEstimate / Object.keys(result).length;
+const remainingDaysOfThisMonth = endOfMonth(yesterday).getDate() - yesterday.getDate();
 const sumCostEstimateOfThisMonth = sumCostEstimate + avgCostEstimate * remainingDaysOfThisMonth;
 const sumCostEstimateOf30Days = avgCostEstimate * 30;
 
 await postDiscordMessage(`\
-## 昨日(${yesterday})の電力使用状況
-${_(result[yesterday].value)} kWh (前日より${_(diffValue)})
-${_(result[yesterday].costEstimate)} 円 (前日より${_(diffCostEstimate)})
+## 昨日(${yesterdayKey})の電力使用状況
+${_(result[yesterdayKey].value)} kWh (前日より${_(diffValue)})
+${_(result[yesterdayKey].costEstimate)} 円 (前日より${_(diffCostEstimate)})
 ## 昨日までの合計料金
 ${_(sumCostEstimate)} 円
 ## 今月の推定料金
